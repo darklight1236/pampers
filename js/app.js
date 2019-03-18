99dfd5031d91404dea5a48e915300a3fb0e8f8f4
DarkLight1236 <bunnyhunter123@yandex.ru>
2019-03-18 16:49:44 +0400

    v3

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('.main_body').fullpage({
+		anchors: ['block1','block2','block3','block4','block5'],
+		menu: '#menu',
+		css3: true,
+		scrollingSpeed: 900
+	});
+});
